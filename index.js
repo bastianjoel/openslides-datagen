ddc47311560f1890102a4d8ff1c01b97fa8d4efa
Bastian Rihm <brihm@intevation.de>
2024-06-26 13:55:29 +0200

    Add constants for data amounts

--- index.js
+++ index.js
@@ -8,19 +8,25 @@ import { faker } from '@faker-js/faker';
 const fileData = readFileSync('./example-data.json');
 const data = JSON.parse(fileData);
 
-for (let i = nextUserId; i <= 2000; i++) {
+const NUM_COMMITTEES = 50;
+const NUM_MEETINGS_PER_COMMITTEE = { min: 1, max: 8 };
+const NUM_USERS = 2000;
+const NUM_USERS_PER_MEETING = { min: 50, max: NUM_USERS };
+
+for (let i = nextUserId; i <= NUM_USERS; i++) {
   addUser(data);
 }
 
-for (let i = 4; i <= 2000; i++) {
+for (let i = 4; i <= NUM_USERS; i++) {
   addMeetingUser(data, 1, i);
 }
 
-for (let cI = 1; cI <= 50; cI++) {
+for (let cI = 2; cI <= NUM_COMMITTEES; cI++) {
   const committeeId = addCommittee(data);
-  for (let mI = 1; mI <= 2; mI++) {
+  const addMeetingsAmount = faker.helpers.rangeToNumber(NUM_MEETINGS_PER_COMMITTEE);
+  for (let mI = 1; mI <= addMeetingsAmount; mI++) {
     const meetingId = addMeeting(data, committeeId);
-    const users = faker.helpers.arrayElements(data[`organization`][`1`][`user_ids`], { min: 50, max: 2000 });
+    const users = faker.helpers.arrayElements(data[`organization`][`1`][`user_ids`], NUM_USERS_PER_MEETING);
     for (let uId of users) {
       addMeetingUser(data, meetingId, uId);
     }
